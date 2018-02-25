3c3cb50900505f8993749b7aab7a6c1af7968eb5
ianfawaz <ian.fawaz@colorado.edu>
2018-02-25 13:21:06 -0700

    app.js for pinging

--- myapp/app.js
+++ myapp/app.js
@@ -9,22 +9,31 @@ var app = express();
 var ingredients = require('recipe-ingredient-parser');
 var str = '100 liters of white milk, 1 cup of water,  2 1/2 cups all-purpose flour'
 var inputStr = str.split(',')
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
 
-app.listen(3000, function () {
-console.log(inputStr)
-  console.log('Example app listening on port 3000!');
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+app.get('/', function (req, res) {
+
+	// var xml = require('xml');
+	var spawn = require("child_process").spawn;
+//var url = 'http://allrecipes.com/Recipe/Apple-Cake-Iv/Detail.aspx';
+	var url = req.query.var1;
+	var pythonProcess = spawn('python3.6',["../recipe-scrapers/run.py", url]);
+	
+	//console.log(req.query.var1)
   
-  pythonProcess.stdout.on('data', function (data){
+ 	pythonProcess.stdout.on('data', function (data){
 	// Do something with the data returned from python script
-		console.log("I'm here");
+		//console.log("I'm here");
 		var arr = JSON.stringify(data.toString('utf8'),null,2).replace(']\\n"','').replace("'\\'","");
 		var arr2 = data.toString('utf8');
 		var arrSplit = arr.split(',')
 
-		console.log(arrSplit[7])
+		//console.log(arrSplit[7])
+		var concatstring = "";
 
 
 // ' \'4 teaspoons ground cinnamon\''
@@ -34,13 +43,33 @@ console.log(inputStr)
 		for (i = 4; i < arrSplit.length; i++) {
 			var string = arrSplit[i].replace(" \'",'').replace("''","").replace("\\","").replace(" ", "").replace("\'","");
     		//console.log(ingredients.parse(arrSplit[i]));
-    		console.log(ingredients.parse(string));
+
+    		//console.log(ingredients.parse(string));
+    		//res.send(ingredients.parse(string));
+    		concatstring = concatstring + JSON.stringify(ingredients.parse(string))
 
     		/*if (i==5){
     			console.log(arrSplit[i]);
     		}*/
 
 		}
+		console.log("ping!!!")
+		res.send(concatstring)
+
+	});
+  });
+
+app.listen(3000, function () {
+//console.log(inputStr)
+  //console.log('Example app listening on port 3000!');
+  
+  pythonProcess.stdout.on('data', function (data){
+	// Do something with the data returned from python script
+		console.log("I'm here");
+		var arr = JSON.stringify(data.toString('utf8'),null,2).replace(']\\n"','').replace("'\\'","");
+		var arr2 = data.toString('utf8');
+		var arrSplit = arr.split(',')
+
 	});
   /*
   for (i = 0; i < inputStr.length; i++) { 
